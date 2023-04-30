cd7ab60fcd414f653fcfde7cd15b4dee110cb4a2
Yang Zhang <yang_zhang@iapcm.ac.cn>
2023-04-30 16:51:54 +0800

    chore: better code (generated with ChatGPT plus)

--- blocks/motor.js
+++ blocks/motor.js
@@ -11,88 +11,139 @@ goog.provide('Blockly.Constants.StepperMotor');
 goog.require('Blockly');
 goog.require('Blockly.Blocks');
 
-Blockly.defineBlocksWithJsonArray([
-{
-  "type": "stepper_motor",
-  "message0": "Stepper Motor %1 pin_en %2 pin_dir %3 pin_stp %4",
-  "args0": [
-    {
-      "type": "field_variable",
-      "name": "VAR",
-      "variable": "motor1"
-    },
-    {
-      "type": "input_value",
-      "name": "PIN_EN"
-    },
-    {
-      "type": "input_value",
-      "name": "PIN_DIR"
-    },
-    {
-      "type": "input_value",
-      "name": "PIN_STP"
-    }
-  ],
-  "output": null,
-  "colour": 230,
-  "tooltip": "Create a stepper motor instance",
-  "helpUrl": ""
-},
-{
-  "type": "forward_motor",
-  "message0": "forward motor %1 with duration %2 frequency %3 duty cycle %4",
-  "args0": [
-    {
-      "type": "field_variable",
-      "name": "MOTOR",
-      "variable": "motor"
-    },
-    {
-      "type": "input_value",
-      "name": "DURATION"
-    },
-    {
-      "type": "input_value",
-      "name": "FREQ"
-    },
-    {
-      "type": "input_value",
-      "name": "DC"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-  "tooltip": "Move motor forward with given duration, frequency, and duty cycle.",
-  "helpUrl": ""
-},
-{
-  "type": "backward_motor",
-  "message0": "backward motor %1 with duration %2 frequency %3 duty cycle %4",
-  "args0": [
-    {
-      "type": "field_variable",
-      "name": "MOTOR",
-      "variable": "motor"
-    },
-    {
-      "type": "input_value",
-      "name": "DURATION"
-    },
-    {
-      "type": "input_value",
-      "name": "FREQ"
-    },
-    {
-      "type": "input_value",
-      "name": "DC"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-  "tooltip": "Move motor backward with given duration, frequency, and duty cycle.",
-  "helpUrl": ""
-},
+Blockly.defineBlocksWithJsonArray(
+[
+  {
+    "type": "create_motor_driver",
+    "message0": "Create Motor Driver: Pin EN %1 Pin DIR %2 Pin STP %3 %4",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "PIN_EN",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "PIN_DIR",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "PIN_STP",
+        "check": "Number"
+      },
+      {
+        "type": "field_variable",
+        "name": "MOTOR_VAR",
+        "variable": "motor"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "Create a motor driver with specified pin connections",
+    "helpUrl": ""
+  },
+  {
+    "type": "drive_motor",
+    "message0": "Drive Motor %1 Duration %2 Freq %3 DC %4 Clockwise %5",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "MOTOR_VAR",
+        "variable": "motor"
+      },
+      {
+        "type": "input_value",
+        "name": "DURATION",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "FREQ",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "DC",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "CLOCKWISE",
+        "check": "Boolean"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "Drive the motor forward or backward for a given time length",
+    "helpUrl": ""
+  },
+  {
+    "type": "forward_motor",
+    "message0": "Drive Motor Forward %1 Duration %2 Freq %3 DC %4",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "MOTOR_VAR",
+        "variable": "motor"
+      },
+      {
+        "type": "input_value",
+        "name": "DURATION",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "FREQ",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "DC",
+        "check": "Number"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "Drive the motor forward for a given time length",
+    "helpUrl": ""
+  },
+  {
+    "type": "backward_motor",
+    "message0": "Drive Motor Backward %1 Duration %2 Freq %3 DC %4",
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "MOTOR_VAR",
+        "variable": "motor"
+      },
+      {
+        "type": "input_value",
+        "name": "DURATION",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "FREQ",
+        "check": "Number"
+      },
+      {
+        "type": "input_value",
+        "name": "DC",
+        "check": "Number"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "Drive the motor backward for a given time length",
+    "helpUrl": ""
+  }
 ]);
--- generators/python/motor.js
+++ generators/python/motor.js
@@ -9,30 +9,45 @@ goog.provide('Blockly.Python.motor');
 
 goog.require('Blockly.Python');
 
-Blockly.JavaScript['stepper_motor'] = function(block) {
-  Blockly.Python.definitions_['from_pimotor_import_*'] = 'from pimotor import *';
-  var var_name = Blockly.Python.variableDB_.getName(block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
-  var pin_en = Blockly.Python.valueToCode(block, 'PIN_EN', Blockly.Python.ORDER_ATOMIC);
-  var pin_dir = Blockly.Python.valueToCode(block, 'PIN_DIR', Blockly.Python.ORDER_ATOMIC);
-  var pin_stp = Blockly.Python.valueToCode(block, 'PIN_STP', Blockly.Python.ORDER_ATOMIC);
-  var code = var_name + ' = StepperMotor(' + pin_en + ', ' + pin_dir + ', ' + pin_stp + ')\n';
+Blockly.Python['create_motor_driver'] = function(block) {
+  var pinEn = Blockly.Python.valueToCode(block, 'PIN_EN', Blockly.Python.ORDER_ATOMIC);
+  var pinDir = Blockly.Python.valueToCode(block, 'PIN_DIR', Blockly.Python.ORDER_ATOMIC);
+  var pinStp = Blockly.Python.valueToCode(block, 'PIN_STP', Blockly.Python.ORDER_ATOMIC);
+  var motorVar = Blockly.Python.variableDB_.getName(block.getFieldValue('MOTOR_VAR'), Blockly.Variables.NAME_TYPE);
+
+  Blockly.Python.definitions_['import_pimotor'] = 'from pimotor import *';
+
+  var code = motorVar + ' = StepperMotor(' + pinEn + ', ' + pinDir + ', ' + pinStp + ')\n';
+  return code;
+};
+
+Blockly.Python['drive_motor'] = function(block) {
+  var motorVar = Blockly.Python.variableDB_.getName(block.getFieldValue('MOTOR_VAR'), Blockly.Variables.NAME_TYPE);
+  var duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+  var freq = Blockly.Python.valueToCode(block, 'FREQ', Blockly.Python.ORDER_ATOMIC);
+  var dc = Blockly.Python.valueToCode(block, 'DC', Blockly.Python.ORDER_ATOMIC);
+  var clockwise = Blockly.Python.valueToCode(block, 'CLOCKWISE', Blockly.Python.ORDER_ATOMIC);
+
+  var code = motorVar + '.drive(' + duration + ', ' + freq + ', ' + dc + ', ' + clockwise + ')\n';
   return code;
 };
 
 Blockly.Python['forward_motor'] = function(block) {
-  var motor = Blockly.Python.variableDB_.getName(block.getFieldValue('MOTOR'), Blockly.Variables.NAME_TYPE);
+  var motorVar = Blockly.Python.variableDB_.getName(block.getFieldValue('MOTOR_VAR'), Blockly.Variables.NAME_TYPE);
   var duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
   var freq = Blockly.Python.valueToCode(block, 'FREQ', Blockly.Python.ORDER_ATOMIC);
   var dc = Blockly.Python.valueToCode(block, 'DC', Blockly.Python.ORDER_ATOMIC);
-  var code = motor + '.forward(' + duration + ', ' + freq + ', ' + dc + ')\n';
+
+  var code = motorVar + '.drive(' + duration + ', ' + freq + ', ' + dc + ', True)\n';
   return code;
 };
 
 Blockly.Python['backward_motor'] = function(block) {
-  var motor = Blockly.Python.variableDB_.getName(block.getFieldValue('MOTOR'), Blockly.Variables.NAME_TYPE);
+  var motorVar = Blockly.Python.variableDB_.getName(block.getFieldValue('MOTOR_VAR'), Blockly.Variables.NAME_TYPE);
   var duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
   var freq = Blockly.Python.valueToCode(block, 'FREQ', Blockly.Python.ORDER_ATOMIC);
   var dc = Blockly.Python.valueToCode(block, 'DC', Blockly.Python.ORDER_ATOMIC);
-  var code = motor + '.backward(' + duration + ', ' + freq + ', ' + dc + ')\n';
+
+  var code = motorVar + '.drive(' + duration + ', ' + freq + ', ' + dc + ', False)\n';
   return code;
 };
